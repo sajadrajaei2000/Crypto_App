beac526b65cfe0ac96518f404654eaf0db0c49b3
sajadrajaei2000 <sajadrajaei2000@gmail.com>
2023-06-30 19:27:54 +0330

    commit 7

--- lib/ui/MainWrapper.dart
+++ lib/ui/MainWrapper.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:project3/ui/Homepage.dart';
 import 'package:project3/ui/MarketViewPage.dart';
 import 'package:project3/ui/ProfilePage.dart';
+import 'package:project3/ui/WatchListPage.dart';
+import 'package:project3/ui/ui_helper/BottomNav.dart';
 
 class MainWrapper extends StatefulWidget {
   const MainWrapper({super.key});
@@ -15,10 +17,18 @@ class _MainWrapperState extends State<MainWrapper> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.compare_arrows_outlined), onPressed: () {}),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNav(controller: _myPage),
       body: PageView(
         controller: _myPage,
-        children: const [Homepage(), MarketViewPage(), ProfilePage()],
+        children: const [
+          Homepage(),
+          MarketViewPage(),
+          ProfilePage(),
+          WatchListPage()
+        ],
       ),
     );
   }
---
+++ lib/ui/ui_helper/BottomNav.dart
@@ -0,0 +1,82 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, must_be_immutable
+
+import 'package:flutter/material.dart';
+
+class BottomNav extends StatelessWidget {
+  PageController controller;
+  BottomNav({super.key, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      height: 63,
+      notchMargin: 5,
+      color: Colors.amber,
+      shape: CircularNotchedRectangle(),
+      child: SizedBox(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 2 - 20,
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      controller.animateToPage(0,
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.easeInOut);
+                    },
+                    icon: Icon(
+                      Icons.home,
+                      size: 28,
+                    )),
+                IconButton(
+                    onPressed: () {
+                      controller.animateToPage(1,
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.easeInOut);
+                    },
+                    icon: Icon(
+                      Icons.bar_chart_rounded,
+                      size: 28,
+                    ))
+              ],
+            ),
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width / 2 - 20,
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      controller.animateToPage(2,
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.easeInOut);
+                    },
+                    icon: Icon(
+                      Icons.person,
+                      size: 28,
+                    )),
+                IconButton(
+                    onPressed: () {
+                      controller.animateToPage(3,
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.easeInOut);
+                    },
+                    icon: Icon(
+                      Icons.panorama_vertical_select,
+                      size: 28,
+                    ))
+              ],
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
