1b9e0a61628b0fd27c30d7bbf82712d2cd14a611
sajadrajaei2000 <sajadrajaei2000@gmail.com>
2023-07-04 18:27:37 +0330

    commit 10

---
+++ lib/ui/providers/CryptoDataProvider.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:project3/network/ApiSupplier.dart';
+
+class CryptoDataProvider extends ChangeNotifier {
+  ApiSupplier apiSupplier = ApiSupplier();
+  getTopMarketCapData() {}
+}
